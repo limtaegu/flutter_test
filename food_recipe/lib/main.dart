e0ab2df02b4db62c66b82fd7042e62335690f30f
limteagu <aka1997@naver.com>
2022-08-06 10:15:26 +0900

    석삼 아이콘 추가

--- food_recipe/lib/main.dart
+++ food_recipe/lib/main.dart
@@ -83,6 +83,7 @@ class HomePage extends StatelessWidget {
       ),
       body: Column(
         children: [
+          /// 검색
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextField(
@@ -102,13 +103,14 @@ class HomePage extends StatelessWidget {
             ),
           ),
           Divider(height: 1),
-          //카테고리 목록
+
+          /// 카테고리 목록
           Expanded(
             child: ListView.builder(
-              //보여주려는 데이터 개수
+              // 보여주려는 데이터 개수
               itemCount: dataList.length,
-              //itemCountn 만큼 반복되며 화면에 보여주려는 위젯
-              // tindex가 0부터 dataList.length - 1 까지 증가하며 전달됩니다.
+              // itemCount 만큼 반복되며 화면에 보여주려는 위젯
+              // index가 0부터 dataList.length - 1까지 증가하며 전달됩니다.
               itemBuilder: (context, index) {
                 // dataList에서 index에 해당하는 data 꺼내기
                 Map<String, dynamic> data = dataList[index];
@@ -118,13 +120,14 @@ class HomePage extends StatelessWidget {
                 // 카드 형태의 위젯
                 return Card(
                   margin: const EdgeInsets.all(8),
-                  //위젯들을 위로 중첩하기 위해 Stack 위젯을 사용
+                  // 위젯들을 위로 중첩하기 위해 Stack 위젯 사용
                   child: Stack(
-                    alignment: Alignment.center, //중앙정렬
+                    alignment: Alignment.center, // 중앙 정렬
                     children: [
+                      /// 배경 이미지
                       Image.network(
                         imgUrl,
-                        width: double.infinity, //가득 채우기
+                        width: double.infinity, // 가득 채우기
                         height: 120,
                         fit: BoxFit
                             .cover, // 이미지 비율을 유지하며 주어진 width와 height를 가득 채우기
@@ -137,7 +140,7 @@ class HomePage extends StatelessWidget {
                         color: Colors.black.withOpacity(0.5), // 투명도 50%
                       ),
 
-                      ///카테고리
+                      /// 카테고리
                       Text(
                         category,
                         style: TextStyle(
@@ -152,6 +155,120 @@ class HomePage extends StatelessWidget {
             ),
           ),
         ],
+      ),
+
+      /// AppBar 왼쪽에 아이콘을 누를 때 왼쪽에서 오른쪽으로 나오는 위젯
+      /// 해당 위젯을 추가하면 AppBar 왼쪽에 아이콘이 자동으로 추가됨
+      drawer: Drawer(
+        child: Column(
+          children: [
+            // Drawer 윗 부분
+            DrawerHeader(
+              margin: const EdgeInsets.all(0),
+              decoration: BoxDecoration(
+                color: Colors.amber,
+              ),
+              child: SizedBox(
+                width: double.infinity, // 가득 채우기
+                child: Column(
+                  children: [
+                    /// 동그란 배경
+                    CircleAvatar(
+                      radius: 36,
+                      backgroundColor: Colors.white,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+
+                        /// 이미지
+                        child: Image.network(
+                          "https://i.ibb.co/CwzHq4z/trans-logo-512.png",
+                          width: 62,
+                        ),
+                      ),
+                    ),
+
+                    /// 닉네임 상단 여백
+                    SizedBox(height: 16),
+
+                    /// 닉네임
+                    Text(
+                      "닉네임",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+
+                    /// 이메일
+                    Text(
+                      "hello@world.com",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            /// 이벤트 배너
+            // 특정 비율로 위젯을 보여주기
+            AspectRatio(
+              aspectRatio: 12 / 4, // 가로 : 세로 = 12 : 4 (이미지를 12:4로 준비했어요)
+              /// 이벤트 배너 리스트
+              /// 스크롤을 하는데, 특정 항목이 스냅이 걸리도록 만들고 싶은 경우 PageView를 사용해요.
+              child: PageView(
+                children: [
+                  Image.network(
+                    "https://i.ibb.co/Q97cmkg/sale-event-banner1.jpg",
+                  ),
+                  Image.network(
+                    "https://i.ibb.co/GV78j68/sale-event-banner2.jpg",
+                  ),
+                  Image.network(
+                    "https://i.ibb.co/R3P3RHw/sale-event-banner3.jpg",
+                  ),
+                  Image.network(
+                    "https://i.ibb.co/LRb1VYs/sale-event-banner4.jpg",
+                  ),
+                ],
+              ),
+            ),
+
+            /// 구매 내역
+            ListTile(
+              title: Text(
+                '구매내역',
+                style: TextStyle(fontSize: 18),
+              ),
+              trailing: Icon(
+                Icons.arrow_forward_ios,
+                color: Colors.black,
+              ),
+              onTap: () {
+                // 클릭시 drawer 닫기
+                Navigator.pop(context);
+              },
+            ),
+
+            /// 레시피 공유하기
+            ListTile(
+              title: Text(
+                '저장한 레시피',
+                style: TextStyle(fontSize: 18),
+              ),
+              trailing: Icon(
+                Icons.arrow_forward_ios,
+                color: Colors.black,
+              ),
+              onTap: () {
+                // 클릭시 drawer 닫기
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
